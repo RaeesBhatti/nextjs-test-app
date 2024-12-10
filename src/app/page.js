aa4bbff55462110b25b94ff2a77bff8a12ae7679
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2024-12-10 12:52:52 -0500

    Add image optimization

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import {headers} from "next/headers";
+import Image from 'next/image';
+import testPic from './test.png';
 
 const getDate = async () => {
     "use server";
-    console.log("getDate called");
     const date = new Date().toISOString();
+    console.log("getDate called", date, 'a really long string that should cause the line to wrap and be truncated', date, 'Another really long string that should cause the line to wrap and be truncated', date);
 
     return date;
 }
@@ -21,6 +23,14 @@ export default async function Home() {
             <h1 className={styles.title}>Welcome to my website</h1>
             <p className={styles.date}>Today is {date}</p>
             <p className={styles.hostname}>Hostname: {hostname}</p>
+            <Image
+                src={testPic}
+                alt="Picture of the author"
+                width={200}
+                height={200}
+                // blurDataURL="data:..." automatically provided
+                // placeholder="blur" // Optional blur-up while loading
+            />
         </div>
     );
 }
