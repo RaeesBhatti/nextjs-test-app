91bc8b74ade79843b07ed466431458dffef73b12
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2025-02-10 15:31:58 -0500

    Update

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,6 +1,6 @@
 // Next.js will invalidate the cache when a
 // request comes in, at most once every 60 seconds.
-export const revalidate = 0;
+export const revalidate = 60;
 
 // We'll prerender only the params from `generateStaticParams` at build time.
 // If a request comes in for a path that hasn't been generated,
@@ -17,8 +17,7 @@ export async function generateStaticParams() {
 export default async function Page(props) {
   const params = await props.params;
   const post = await fetch(`https://api.vercel.app/blog/${params.id}`, {
-    next: { tags: [`blog${params.id}`], revalidate: 60 },
-    cache: "force-cache",
+    next: { tags: [`blog${params.id}`] },
   }).then((res) => res.json());
   return (
     <main>
